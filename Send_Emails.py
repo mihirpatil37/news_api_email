47dfe3655f149127248efad976ba29a730c4fecb
MIhir Pradip Patil <patilmihir9588@gmail.com>
2025-03-12 13:06:54 +0100

    Update Send_Emails.py

--- Send_Emails.py
+++ Send_Emails.py
@@ -7,10 +7,12 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "patilmihir9588@gmail.com"
-    password = "xplnkbupijisicox"
-
-    receiver = "patilmihir958@gmail.com"
+    # Create a password for your email.(gmail or yahoo)
+    username = ""
+    password = ""
+    
+    # Add a reciever email.
+    receiver = ""
     my_context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=my_context) as server :
